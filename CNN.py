8197ecb7779c7526c398405102be81b88f9b1d3d
Ryoma Ke <kalryoma@gmail.com>
2018-05-08 16:19:54 +1000

    add arguments for input training data file and output SavedModel path

--- CNN.py
+++ CNN.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import numpy as np
 import tensorflow as tf
+import argparse
+import sys
 
 def load_training_data(file_path):
     data = pd.read_csv(file_path)
@@ -48,75 +50,89 @@ def new_batch():
         return total_x[start:end], total_y[start:end]
     return get_batch
 
-pixels, labels = load_training_data("/Users/kalryoma/Downloads/fer2013/fer2013.csv")
-pixels_amount = pixels.shape[1]
-labels_count = np.unique(labels).shape[0]
-width = height = np.ceil(np.sqrt(pixels_amount)).astype(np.uint8)
-train_pixels, train_labels = pre_processing(pixels, labels)
+if __name__=="__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-input", help="the input file training data path")
+    parser.add_argument("-output", help="the output SavedModel path") 
+    args = parser.parse_args()
+    data_file = args.input
+    if (not data_file):
+        print("no input data")
+        sys.exit()
+    model_path = args.output
+    if (not data_file):
+        print("no output path")
+        sys.exit()
 
-#Build CNN Model
-# input:x,  output: y_
-x = tf.placeholder('float', shape=[None, pixels_amount])#(28709, 2304)
-y_ = tf.placeholder('float', shape=[None, labels_count])#(28709, 7)
-# first convolutional layer 64
-w_conv1 = init_weight([5, 5, 1, 64], False)
-b_conv1 = init_bias([64], False)
-image = tf.reshape(x, [-1, width, height, 1])#(28709,48,48,1)
-h_conv1 = tf.nn.relu(b_conv1+tf.nn.conv2d(image, w_conv1, strides=[1, 1, 1, 1], padding="SAME"))#(28709,48,48,64)
-h_pool1 = tf.nn.max_pool(h_conv1, ksize=[1, 3, 3, 1], strides=[1, 2, 2, 1], padding='SAME')#(28709,24,24,64)
-h_norm1 = tf.nn.lrn(h_pool1, 4, bias=1.0, alpha=0.001/9.0, beta=0.75)#(28709,24,24,64)
-# second convolutional layer
-w_conv2 = init_weight([5, 5, 64, 128], False)
-b_conv2 = init_bias([128], False)
-h_conv2 = tf.nn.relu(b_conv2+tf.nn.conv2d(h_norm1, w_conv2, strides=[1, 1, 1, 1], padding="SAME"))#(28709,24,24,128)
-h_norm2 = tf.nn.lrn(h_conv2, 4, bias=1.0, alpha=0.001/9.0, beta=0.75)#(28709,24,24,128)
-h_pool2 = tf.nn.max_pool(h_norm2, ksize=[1, 3, 3, 1], strides=[1, 2, 2, 1], padding='SAME')#(28709, 12, 12, 128)
-# densely connected layer local 3
-w_fc1 = init_weight([12*12*128, 3072], True)
-b_fc1 = init_bias([3072], True)
-h_fc1 = tf.nn.relu(b_fc1+tf.matmul(tf.reshape(h_pool2, [-1, 12*12*128]), w_fc1))#(28709, 3702)
-# densely connected layer local 4
-w_fc2 = init_weight([3072, 1536], True)
-b_fc2 = init_bias([1536], True)
-h_fc2 = tf.nn.relu(b_fc2+tf.matmul(tf.reshape(h_fc1, [-1, 3072]), w_fc2))#(28709, 1536)
-# dropout
-drop_prob = tf.placeholder('float')
-h_fc2_drop = tf.nn.dropout(h_fc2, 1-drop_prob)
-# readout layer for deep net
-w_fc3 = init_weight([1536, labels_count], False)
-b_fc3 = init_bias([labels_count], False)
-y = tf.nn.softmax(tf.matmul(h_fc2_drop, w_fc3) + b_fc3)#(28709, 7)
-# prediction function
-predict = tf.argmax(y, 1)
+    pixels, labels = load_training_data(data_file)
+    pixels_amount = pixels.shape[1]
+    labels_count = np.unique(labels).shape[0]
+    width = height = np.ceil(np.sqrt(pixels_amount)).astype(np.uint8)
+    train_pixels, train_labels = pre_processing(pixels, labels)
 
-LEARNING_RATE = 1e-4
-# cost function
-cross_entropy = -tf.reduce_sum(y_*tf.log(y))
-# optimization function
-train_step = tf.train.AdamOptimizer(LEARNING_RATE).minimize(cross_entropy)
-# evaluation
-accuracy = tf.reduce_mean(tf.cast(tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1)), 'float'))
+    #Build CNN Model
+    # input:x,  output: y_
+    x = tf.placeholder('float', shape=[None, pixels_amount])#(28709, 2304)
+    y_ = tf.placeholder('float', shape=[None, labels_count])#(28709, 7)
+    # first convolutional layer 64
+    w_conv1 = init_weight([5, 5, 1, 64], False)
+    b_conv1 = init_bias([64], False)
+    image = tf.reshape(x, [-1, width, height, 1])#(28709,48,48,1)
+    h_conv1 = tf.nn.relu(b_conv1+tf.nn.conv2d(image, w_conv1, strides=[1, 1, 1, 1], padding="SAME"))#(28709,48,48,64)
+    h_pool1 = tf.nn.max_pool(h_conv1, ksize=[1, 3, 3, 1], strides=[1, 2, 2, 1], padding='SAME')#(28709,24,24,64)
+    h_norm1 = tf.nn.lrn(h_pool1, 4, bias=1.0, alpha=0.001/9.0, beta=0.75)#(28709,24,24,64)
+    # second convolutional layer
+    w_conv2 = init_weight([5, 5, 64, 128], False)
+    b_conv2 = init_bias([128], False)
+    h_conv2 = tf.nn.relu(b_conv2+tf.nn.conv2d(h_norm1, w_conv2, strides=[1, 1, 1, 1], padding="SAME"))#(28709,24,24,128)
+    h_norm2 = tf.nn.lrn(h_conv2, 4, bias=1.0, alpha=0.001/9.0, beta=0.75)#(28709,24,24,128)
+    h_pool2 = tf.nn.max_pool(h_norm2, ksize=[1, 3, 3, 1], strides=[1, 2, 2, 1], padding='SAME')#(28709, 12, 12, 128)
+    # densely connected layer local 3
+    w_fc1 = init_weight([12*12*128, 3072], True)
+    b_fc1 = init_bias([3072], True)
+    h_fc1 = tf.nn.relu(b_fc1+tf.matmul(tf.reshape(h_pool2, [-1, 12*12*128]), w_fc1))#(28709, 3702)
+    # densely connected layer local 4
+    w_fc2 = init_weight([3072, 1536], True)
+    b_fc2 = init_bias([1536], True)
+    h_fc2 = tf.nn.relu(b_fc2+tf.matmul(tf.reshape(h_fc1, [-1, 3072]), w_fc2))#(28709, 1536)
+    # dropout
+    drop_prob = tf.placeholder('float')
+    h_fc2_drop = tf.nn.dropout(h_fc2, 1-drop_prob)
+    # readout layer for deep net
+    w_fc3 = init_weight([1536, labels_count], False)
+    b_fc3 = init_bias([labels_count], False)
+    y = tf.nn.softmax(tf.matmul(h_fc2_drop, w_fc3) + b_fc3)#(28709, 7)
+    # prediction function
+    predict = tf.argmax(y, 1)
 
-# start TensorFlow session
-init = tf.global_variables_initializer()
-sess = tf.InteractiveSession()
-sess.run(init)
+    LEARNING_RATE = 1e-4
+    # cost function
+    cross_entropy = -tf.reduce_sum(y_*tf.log(y))
+    # optimization function
+    train_step = tf.train.AdamOptimizer(LEARNING_RATE).minimize(cross_entropy)
+    # evaluation
+    accuracy = tf.reduce_mean(tf.cast(tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1)), 'float'))
 
-MAX_ITERATIONS = 100
-DROPOUT_PROB = 0.5
-BATCH_SIZE = 50
-display_step = 1
-get_new_batch = new_batch()
-for i in range(MAX_ITERATIONS):
-    batch_x, batch_y = get_new_batch(train_pixels, train_labels, BATCH_SIZE)
-    # check performance in the process
-    if (i % display_step)==0 or i==(MAX_ITERATIONS-1):
-        if (i == 10 or i == 100) and display_step < 100:
-            display_step *= 10
-        train_accuracy = accuracy.eval(feed_dict={x: batch_x, y_: batch_y, drop_prob: 0})
-        print('step %d: accuracy=%.2f' % (i, train_accuracy))
-    sess.run(train_step, feed_dict={x: batch_x, y_: batch_y, drop_prob: DROPOUT_PROB})
+    # start TensorFlow session
+    init = tf.global_variables_initializer()
+    sess = tf.InteractiveSession()
+    sess.run(init)
 
-# builder = tf.saved_model.builder.SavedModelBuilder('/Users/kalryoma/Downloads/my-model')
-# builder.add_meta_graph_and_variables(sess,[tf.saved_model.tag_constants.TRAINING])
-# builder.save()
+    MAX_ITERATIONS = 10
+    DROPOUT_PROB = 0.5
+    BATCH_SIZE = 50
+    display_step = 1
+    get_new_batch = new_batch()
+    for i in range(MAX_ITERATIONS):
+        batch_x, batch_y = get_new_batch(train_pixels, train_labels, BATCH_SIZE)
+        # check performance in the process
+        if (i % display_step)==0 or i==(MAX_ITERATIONS-1):
+            if (i == 10 or i == 100) and display_step < 100:
+                display_step *= 10
+            train_accuracy = accuracy.eval(feed_dict={x: batch_x, y_: batch_y, drop_prob: 0})
+            print('step %d: accuracy=%.2f' % (i, train_accuracy))
+        sess.run(train_step, feed_dict={x: batch_x, y_: batch_y, drop_prob: DROPOUT_PROB})
+
+    builder = tf.saved_model.builder.SavedModelBuilder(model_path)
+    builder.add_meta_graph_and_variables(sess,[tf.saved_model.tag_constants.TRAINING])
+    builder.save()
